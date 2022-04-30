4a5be91617d2cb286100da9eaa1129712bf36f52
Mohammed Rabil <rabil@techie.com>
2022-05-01 03:43:39 +0530

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { Deta } from "deta";
 
-const deta = Deta(process.env.DETA_PROJECT_KEY);
+const deta = Deta("Your Deta Project Key");
 
 const db = deta.Base("deta-remix");
 
@@ -22,8 +22,8 @@ export const action: ActionFunction = async ({ request }) => {
       return key
         ? await db.delete(key as string)
         : { error: "key is required" };
-      default:
-        return { error: "unknown action" };
+    default:
+      return { error: "unknown action" };
   }
 };
 
@@ -38,11 +38,7 @@ export default function Index() {
       <h1>Welcome to Remix</h1>
       <p>Fun fact: this app can also function without JavaScript</p>
       <div>
-        {res.items.map((d: {
-          key: string;
-          name: string;
-          message: string;
-        }) => {
+        {res.items.map((d: { key: string; name: string; message: string }) => {
           return (
             <div
               key={d.key}
@@ -63,21 +59,19 @@ export default function Index() {
       </div>
       <Form method="post" style={{ padding: "1rem 0" }}>
         <p>
-          <label htmlFor="name">
-            Name
-          </label>
+          <label htmlFor="name">Name</label>
           <br />
-            <input type="text" name="name" />
+          <input type="text" name="name" />
         </p>
         <p>
-          <label htmlFor="message">
-            Message
-          </label>
+          <label htmlFor="message">Message</label>
           <br />
-            <input type="text" name="message" />
+          <input type="text" name="message" />
         </p>
         <p>
-          <button name="_action" value="create">Add</button>
+          <button name="_action" value="create">
+            Add
+          </button>
         </p>
       </Form>
     </div>
